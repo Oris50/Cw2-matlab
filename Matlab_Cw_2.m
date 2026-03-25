f587c5019a0ada5a9e66c00993caf346fe177c48
ggora <ggora@Batmanovo-Oris>
2026-03-25 14:13:16 +0000

    Preliminary complete

--- Matlab_Cw_2.m
+++ Matlab_Cw_2.m
@@ -1,1 +1,18 @@
- disp('hello')
+% Oraibi George-Gudi
+% Efyog1@nottingham.ac.uk
+
+%% Prelimanry Task 
+clear
+
+a = arduino('COM9','Uno') % This is to be able to create a arduino
+
+%Creates a loop that makes LED blink for 10 seconds
+
+for t= 1:10
+
+writeDigitalPin(a,'d11',1) %This is to provide the connection with the arduino pin 
+%with the LED while applying a 5V tension to the LED
+pause(0.5)
+writeDigitalPin(a,'d11,0')
+pause(0.5)
+end

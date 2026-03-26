d3c4e6a3003429ed53f07d72a145a4874bc4576e
Oris50 <ggOraibi@gmail.com>
2026-03-26 14:22:22 +0000

    done

--- temp_monitor.m
+++ temp_monitor.m
@@ -1,5 +1,5 @@
 function  temp_monitor(a)
-
+% Assign arduino digital pins for LEDS
 Green_LED = 'D11';
 yellow_LED = 'D10';
 red_LED = 'D12';
@@ -11,13 +11,13 @@ time_data =[];
 t = 0;
 start_Time = tic;
 
-loop = true;
-figure
+loop = true; % allows the loop
+figure % Creates the figures for live plottings
 while loop==true
-    Voltage = readVoltage(a,"A0");
+    Voltage = readVoltage(a,"A0"); 
     temp = (Voltage-0.5)/0.01;
-
-    time_data = [time_data,t];
+%Both time and temperature values become arrays
+    time_data = [time_data,t]; 
     temp_data= [temp_data,temp];
 
     %Plotting graph
@@ -27,20 +27,26 @@ while loop==true
     ylabel('Temperature (°C)');
     title('Capsule Temperature Monitoring')
     grid on;
-    drawnow;
+    drawnow; %Makes an immediate plot update
 
-    if temp>=18 && temp<=24
+    if temp>=18 && temp<=24 % Temperature for 
        writeDigitalPin(a, Green_LED, 1)
         writeDigitalPin(a, yellow_LED, 0);
         writeDigitalPin(a, red_LED, 0);
     elseif temp < 18
         writeDigitalPin(a, Green_LED, 0);
-        writeDigitalPin(a, yellow_LED, 1);
         writeDigitalPin(a, red_LED, 0);
+        writeDigitalPin(a,yellow_LED, 1);
+        pause(0.5);
+        writeDigitalPin(a,yellow_LED,0)
+        pause(0.5)
     else
         writeDigitalPin(a, Green_LED, 0);
         writeDigitalPin(a, yellow_LED, 0);
         writeDigitalPin(a, red_LED, 1);
+        pause(0.25)
+        writeDigitalPin(a,red_LED,0)
+        pause(0.25)
     end
 
     t = t + 1; % Increment time
--- temp_prediction.m
+++ temp_prediction.m
@@ -1,19 +1,27 @@
+%This function reads the votlage from the thermistor then calculates the
+%rate in change of temperature , dependendent on that rate of change this
+%function will check whether its too high, in range or too low which will
+%cause an LED to light up respectively. The function also predcits the
+%temperature in the next 5 minutes from the current temperature rate of
+%change.
+
 function  temp_prediction(a)
-  
+
+ %This is to assign my LEDs to my arduinos digital pin
 Green_LED = 'D11';
 yellow_LED = 'D10';
 red_LED = 'D12';
 
-prev_temp = [];
-loop = true;
-start_time = tic;
+prev_temp = []; % Stores the previous temperature reading
+loop = true; %allowing loop to run
+start_time = tic; %Timer starts for measuring elasped time during iterations
 
 while loop==true
 
-    voltage=readVoltage(a,'A0');
-    temp = (voltage - 0.5)*100;
+    voltage=readVoltage(a,'A0'); % Read voltage from analog pin A0
+    temp = (voltage - 0.5)*100; % Equation turning voltage to temperature.
 
-    if isempty(prev_temp)
+    if isempty(prev_temp) % At rate=0 nothing can be calculated
         rate = 0;
     else
         rate = (temp - prev_temp)/6;
@@ -22,27 +30,27 @@ while loop==true
     prev_temp = temp; % Update previous temperature for the next iteration
 predicted_temp = temp + (rate * 300);
 elapsed = toc(start_time);
-
+%Displaying readings and calculations
 fprintf('Time: %.2f s\n', elapsed);
 fprintf('Current Temperature: %.2f °C\n',  temp)
 fprintf('Rate of Change: %.2f °C/min\n',rate);
 fprintf('Predicted Temperature (5mins): %.2f °C\n', predicted_temp)
 
 % Control LEDs based on temperature prediction
-if rate > (4/60)
+if rate > (4/60) %If statement for if temperature is rising faster than 4degrees
     writeDigitalPin(a, red_LED, 1); % Turn on red LED
     writeDigitalPin(a, Green_LED, 0); % Turn off green LED
     writeDigitalPin(a, yellow_LED, 0); % Turn off yellow LED
-elseif rate < -(4/60)
+elseif rate < -(4/60) %If statement is dropping faster than -4degrees a minute
     writeDigitalPin(a, yellow_LED, 1); % Turn on yellow LED
     writeDigitalPin(a, Green_LED, 0); % Turn off green LED
     writeDigitalPin(a, red_LED, 0); % Turn off red LED
-elseif temp >= 18 && temp<=24
+elseif temp >= 18 && temp<=24 %If temp is between 18 to 24
     writeDigitalPin(a, Green_LED, 1); % Turn on green LED
     writeDigitalPin(a, yellow_LED, 0); % Turn off yellow LED
     writeDigitalPin(a, red_LED, 0); % Turn off red LED
 
-else
+else %NO LEDs turn on
      writeDigitalPin(a, Green_LED, 0); 
     writeDigitalPin(a, yellow_LED, 0); 
     writeDigitalPin(a, red_LED, 0);
